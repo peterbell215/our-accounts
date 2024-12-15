869ef581e0321a7db9a00f9e899d11503ce33ede
Peter Bell <peter.bell215@gmail.com>
2024-12-15 19:16:44 +0000

    Added more details to Account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,8 @@
 class Account < ApplicationRecord
+  has_many :transactions
+
+  monetize :opening_balance_pence
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
+  validates :opening_balance, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_14_182239) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_15_164411) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "account_number"
+    t.string "sortcode"
+    t.date "opening_date"
+    t.integer "opening_balance_pence", default: 0, null: false
+    t.string "opening_balance_currency", default: "GBP", null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -32,12 +37,17 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_14_182239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+    t.integer "previous_trx_id", null: false
+    t.integer "balance_pence", default: 0, null: false
+    t.string "balance_currency", default: "GBP", null: false
     t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["creditor_id"], name: "index_transactions_on_creditor_id"
     t.index ["debitor_id"], name: "index_transactions_on_debitor_id"
+    t.index ["previous_trx_id"], name: "index_transactions_on_previous_trx_id"
   end
 
   add_foreign_key "transactions", "accounts", column: "creditor_id"
   add_foreign_key "transactions", "accounts", column: "debitor_id"
   add_foreign_key "transactions", "categories"
+  add_foreign_key "transactions", "transactions", column: "previous_trx_id"
 end
