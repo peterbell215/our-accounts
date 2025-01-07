e2b434181db36f19d83bc1af5e3800bc9d810088
Peter Bell <peter.bell215@gmail.com>
2025-01-07 19:30:01 +0000

    Add Barclaycard to project

--- app/models/imported_transaction_factory.rb
+++ app/models/imported_transaction_factory.rb
@@ -5,6 +5,7 @@ require 'csv'
 class ImportedTransactionFactory
   # @param [CSV::Row] csv_row
   # @param [ImportColumnsDefinition] import_columns_definition
+  # @return [null, ImportedTransaction]
   def self.build(csv_row, import_columns_definition)
     imported_transaction = ImportedTransaction.new
     # TODO: replace with something that also checks the account columns if defined.
@@ -15,18 +16,25 @@ class ImportedTransactionFactory
     imported_transaction.trx_type = csv_row[import_columns_definition.transaction_type_column]
     imported_transaction.description = csv_row[import_columns_definition.other_party_column]
 
-    imported_transaction.amount =
-      Money.from_amount(
-        if (import_columns_definition.amount_column)
-          csv_row[import_columns_definition.amount_column]
-        else
-          csv_row[import_columns_definition.credit_column] || -csv_row[import_columns_definition.debit_column]
-        end
-      )
+    imported_transaction.amount = set_amount(csv_row, import_columns_definition)
     imported_transaction.balance =
       import_columns_definition.balance_column && Money.from_amount(csv_row[import_columns_definition.balance_column])
 
     imported_transaction
   end
 
+  private
+
+  # Determine the transaction amount.
+  # @param [CSV::Row] csv_row
+  # @param [ImportColumnsDefinition] import_columns_definition
+  # @return [Money]
+  def self.set_amount(csv_row, import_columns_definition)
+    amount = if (import_columns_definition.amount_column)
+               csv_row[import_columns_definition.amount_column]
+             else
+               csv_row[import_columns_definition.credit_column] || -csv_row[import_columns_definition.debit_column]
+             end
+    Money.from_amount(amount)
+  end
 end
--- spec/factories/import_columns_definitions.rb
+++ spec/factories/import_columns_definitions.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     factory :lloyds_import_columns_definition do
       account factory: :lloyds_account
 
-      date_column { 0 }
+      date_column             { 0 }
       date_format             { "%d/%m/%Y" }
       transaction_type_column { 1 }
       sortcode_column         { 2 }
@@ -14,6 +14,22 @@ FactoryBot.define do
       debit_column            { 5 }
       credit_column           { 6 }
       balance_column          { 7 }
+    end
+
+    factory :barclaycard_import_columns_definition do
+      account factory :barclay_card_account
+
+      date_column             { 0 }
+      date_format             { "%d %b %y" }
+      other_party_column      { 1 }
+      transaction_type_column { 4 }
+      sortcode_column         { nil }
+      account_number_column   { nil }
+
+      amount_column           { nil }
+      debit_column            { 5 }
+      credit_column           { 6 }
+      balance_column          { nil }
     end
   end
 end
