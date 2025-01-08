2c8b90b98fedb30c8680274e31bfdc42f4f752f1
Peter Bell <peter.bell215@gmail.com>
2025-01-08 20:09:09 +0000

    Added test that Lloyds import throws error if account details don't match

--- spec/models/imported_transaction_factory_spec.rb
+++ spec/models/imported_transaction_factory_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-require 'rails_helper'
-
 RSpec.describe ImportedTransactionFactory, type: :model do
   describe 'Lloyds Import' do
+    subject(:imported_transaction) { ImportedTransactionFactory.build(csv_row, import_columns_definition) }
+
     let(:import_columns_definition) { FactoryBot.create(:lloyds_import_columns_definition) }
 
     let(:csv_row) { CSV::Row.new(header, csv_data) }
     let(:header) { ["Transaction Date", "Transaction Type", "Sort Code", "Account Number", "Transaction Description", "Debit Amount", "Credit Amount", "Balance"] }
-    let(:csv_data) { ["12/12/2024", "DEB","'30-00-00" ,"01234567" , "Maison Bertaux", 5.95, nil, 1525.80] }
     let(:account) { Account.find_by(name: "Lloyds Account") }
 
     describe '#build' do
-      subject(:imported_transaction) { ImportedTransactionFactory.build(csv_row, import_columns_definition) }
+      let(:csv_data) { ["12/12/2024", "DEB","'30-00-00" ,"01234567" , "Maison Bertaux", 5.95, nil, 1525.80] }
 
       specify { expect(imported_transaction.import_account_id).to eq(account.id) }
       specify { expect(imported_transaction.date).to eq(Date.new(2024, 12, 12)) }
@@ -21,6 +20,14 @@ RSpec.describe ImportedTransactionFactory, type: :model do
       specify { expect(imported_transaction.amount).to eq(Money.from_amount(-5.95)) }
       specify { expect(imported_transaction.balance).to eq(Money.from_amount(1525.80)) }
     end
+
+  describe '#build raises AccountError' do
+    let(:csv_data) { ["12/12/2024", "DEB","'30-00-01" ,"01234567" , "Maison Bertaux", 5.95, nil, 1525.80] }
+
+    it 'raises an AccountError when account details do not match' do
+      expect { ImportedTransactionFactory.build(csv_row, import_columns_definition) }.to raise_error(ImportError, "Sortcode and/or account number do not match with input file")
+    end
+  end
   end
 
   describe 'Barclaycard Import' do
