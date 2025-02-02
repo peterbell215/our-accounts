a80b5c70bbf71dd331bbe1b34102695ca7f2513c
Peter Bell <peter.bell215@gmail.com>
2025-02-02 17:36:28 +0000

    Remove FileImporter#import_row as ImportedTransactionFactory#build does the same

--- app/models/file_importer.rb
+++ app/models/file_importer.rb
@@ -15,36 +15,8 @@ class FileImporter
   # @return [void]
   def read
     CSV.read(@file, headers: true).each do |row|
-      imported_transaction = import_row(row)
+      # @todo: add inner workings.
 
     end
-  end
-
-  # Import a single row of CSV data and generate an ImportedTransaction object with the data in a normalised form.
-  # @param [CSV::Row] row
-  # @return [ImportedTransaction]
-  def import_row(row)
-    imported_transaction = ImportedTransaction.new
-    imported_transaction.date = Date.strptime(row[column_definitions.date_column], column_definitions.date_format)
-    imported_transaction.import_account_id = account.id
-
-    imported_transaction.description = row[column_definitions.other_party_column]
-    imported_transaction.trx_type = row[column_definitions.transaction_type_column]
-
-    if column_definitions.debit_column && column_definitions.credit_column
-      imported_transaction.amount = if row[column_definitions.debit_column]
-                                      Money.from_amount(-row[column_definitions.debit_column].to_f)
-                                    elsif row[column_definitions.credit_column]
-                                      Money.from_amount(row[column_definitions.credit_column].to_f)
-                                    else
-                                      raise ImportError, "No credit or debit amount specified."
-                                    end
-    end
-
-    if column_definitions.balance_column
-      imported_transaction.balance = Money.from_amount(row[column_definitions.balance_column].to_f)
-    end
-
-    imported_transaction
   end
 end
--- spec/models/file_importer_spec.rb
+++ spec/models/file_importer_spec.rb
@@ -1,24 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe FileImporter, type: :class do
-  subject { FileImporter.new('test.csv', lloyds_account) }
-
-  let(:lloyds_account) { FactoryBot.create(:lloyds_account, with_import_columns_definition: true) }
-
-  describe '#import_rows' do
-    let(:csv_row) { CSV::Row.new(header, csv_data) }
-    let(:header) { ["Transaction Date", "Transaction Type", "Sort Code", "Account Number", "Transaction Description", "Debit Amount", "Credit Amount", "Balance"] }
-    let(:csv_data) { ["12/12/2024", "DEB","'30-00-00" ,"00000000" , "Maison Bertaux", 5.95, nil, 1525.80] }
-
-    context 'when we provide a valid row' do
-      let(:imported_transaction) { subject.import_row(csv_row) }
-
-      specify { expect(imported_transaction.import_account_id).to eq(lloyds_account.id) }
-      specify { expect(imported_transaction.date).to eq Date.new(2024,12,12) }
-      specify { expect(imported_transaction.description).to eq("Maison Bertaux") }
-      specify { expect(imported_transaction.trx_type).to eq("DEB") }
-      specify { expect(imported_transaction.amount).to eq Money.from_amount(-5.95) }
-      specify { expect(imported_transaction.balance).to eq Money.from_amount(1525.80) }
-    end
-  end
+  pending "still need to write the tests"
 end
