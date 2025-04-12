2743a22c97611f3143583f90ef69c55812a09f0e
Peter Bell <peter.bell5@ntlworld.com>
2025-04-12 21:16:33 +0100

    Added validation for credit card number formats

--- app/models/credit_card_account.rb
+++ app/models/credit_card_account.rb
@@ -1,4 +1,8 @@
 # Class to represent a credit card account
 class CreditCardAccount < Account
   validates :opening_balance, presence: true
+  validates :account_number, format: {
+    with: /\A\d{4}([- ]?)\d{4}\1\d{4}\1\d{4}\z/,
+    message: "must be a valid credit card number format (e.g. 1234-5678-9012-3456 or 1234 5678 9012 3456)"
+  }
 end
--- spec/models/credit_card_account_spec.rb
+++ spec/models/credit_card_account_spec.rb
@@ -8,7 +8,9 @@ describe CreditCardAccount, type: :model do
   end
 
   describe 'validations' do
-    specify { expect(FactoryBot.build(:lloyds_account, opening_balance: nil)).to_not be_valid }
-    specify { expect(FactoryBot.build(:lloyds_account, account_number: '00')).to_not be_valid }
+    specify { expect(FactoryBot.build(:barclay_card_account, opening_balance: nil)).to_not be_valid }
+    specify { expect(FactoryBot.build(:barclay_card_account, account_number: '00')).to_not be_valid }
+    specify { expect(FactoryBot.build(:barclay_card_account, account_number: '1234-5678-9012-3456')).to be_valid }
+    specify { expect(FactoryBot.build(:barclay_card_account, account_number: '1234 5678 9012 3456')).to be_valid }
   end
 end
