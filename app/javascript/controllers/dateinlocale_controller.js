b2fb5d9a52d2de54f529be171698816213080bd3
Peter Bell <peter.bell5@ntlworld.com>
2025-04-12 20:53:57 +0100

    Generate the DateTimeFormatter only once

--- app/javascript/controllers/dateinlocale_controller.js
+++ app/javascript/controllers/dateinlocale_controller.js
@@ -1,5 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
+const dateTimeFormatter = new Intl.DateTimeFormat(navigator.language, {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+});
+
+// This controller formats a date string in the browser's locale
 export default class extends Controller {
     static targets = [ "date" ];
 
@@ -7,11 +14,7 @@ export default class extends Controller {
         const rawDate = this.element.dataset.date;
 
         // Format the date in the browser's locale
-        const formattedDate = new Intl.DateTimeFormat(navigator.language, {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-        }).format(new Date(rawDate));
+        const formattedDate = dateTimeFormatter.format(new Date(rawDate));
 
         // Update the element with the formatted date
         this.dateTarget.innerHTML = formattedDate;
