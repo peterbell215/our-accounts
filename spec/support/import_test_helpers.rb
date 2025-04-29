93c147ad02cd67736b115db5cebdba78d2c2f010
Peter Bell <peter.bell5@ntlworld.com>
2025-04-29 06:11:13 +0100

    Refactors test file generation for account imports
    Simplifies and improves the test file generation process by using a parameterized filename based on the account name.
    
    This change reduces duplication and enhances the clarity of the test setup.

--- spec/support/import_test_helpers.rb
+++ spec/support/import_test_helpers.rb
@@ -1,21 +1,21 @@
 module ImportTestHelpers
-  LLOYDS_FILENAME = "lloyds_import_file.csv"
-  BARCLAYCARD_FILENAME = "barclaycard_import_file.csv"
-
+  # @param account [Account] The account to generate a filename for
+  # @return [String] A parameterized string based on the account name
   def self.get_filename_for_account(account)
-    case account.name
-    when /Barclaycard/i
-      BARCLAYCARD_FILENAME
-    else
-      LLOYDS_FILENAME
-    end
+    "#{account.name.parameterize}.csv"
   end
 
+  # @param account [Account] The account to generate a path for
+  # @return [Pathname] Full path including filename for the account
   def self.get_filename_with_path(account)
     filename = get_filename_for_account(account)
     Rails.root.join('tmp', filename)
   end
 
+  # Generates a test file with account transaction data
+  # @param account [Account] The account to generate transactions for
+  # @param import_columns_definition_factory [Symbol, nil] Optional factory for column definitions
+  # @return [Pathname] The path to the generated file
   def self.generate_test_file(account, import_columns_definition_factory: nil)
     filename_with_path = get_filename_with_path(account)
 
@@ -27,6 +27,9 @@ module ImportTestHelpers
     filename_with_path
   end
 
+  # Deletes the test file for the given account
+  # @param account [Account] The account whose test file should be removed
+  # @return [Boolean] Result of the file removal operation
   def self.cleanup_test_file(account)
     filename_with_path = get_filename_with_path(account)
     FileUtils.rm_f(filename_with_path)
